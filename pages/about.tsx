8870d1dd400d975d7143878ecfe1ec05350cd0fc
Kota Uchino <k.uchino@everyleaf.com>
2023-08-15 21:23:01 +0900

    Add aticle

--- pages/about.tsx
+++ pages/about.tsx
@@ -11,18 +11,16 @@ const ShowSiteGuide: NextPage = () => {
       </Head>
       <article className="markdown">
         <h1>{pageTitle}</h1>
-        <p></p>
+        <p>
+          このウェブサイトは、個人的な生活や技術に関する記録を残すものです。
+        </p>
         <h2>作者情報</h2>
         <p>
           ソフトウェア開発をしています。
           主にRuby/Railsを使用したWebアプリケーション開発に関わっています。
         </p>
         <p>
-          このサイトは、個人的な生活や技術のブログとして運営しています。
-        </p>
-        <p>
-          GitHub:{" "}
-          <a href="https://github.com/uchinokot">uchinokot</a>
+          GitHub: <a href="https://github.com/uchinokot">uchinokot</a>
         </p>
       </article>
     </>
